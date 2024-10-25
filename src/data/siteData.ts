c48a0208d7de9ff4c01832c1b6eb77d93dbe08ba
Rol4nd <mail@rolandfranke.nl>
2024-10-25 09:51:14 +0200

    Add some more abstraction in layout components

---
+++ src/data/siteData.ts
@@ -0,0 +1,5 @@
+export const baseData = {
+  title: 'Roland Franke - Frontend Developer',
+  description:
+    "Roland Franke's personal blog. Discover a creative frontend developer with expertise in HTML, CSS, React, and accessibility, dedicated to build inclusive and innovative web experiences. Explore projects, professional experience, and insights on modern web development.",
+}
