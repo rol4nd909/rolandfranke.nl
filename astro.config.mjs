54431b4008f9aa64f76cb73f728957ec2a4d93d5
Rol4nd <mail@rolandfranke.nl>
2024-10-29 15:40:03 +0100

    Added sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rolandfranke.nl',
+  integrations: [icon(), sitemap()],
   markdown: {
     syntaxHighlight: false
-  },
-  integrations: [icon()]
+  }
 });
