e40d2104c4cbf8f0c4768dfd2e835c0867f07a5c
Rol4nd <mail@rolandfranke.nl>
2024-10-29 22:23:13 +0100

    Added link component, switched to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rolandfranke.nl',
-  integrations: [icon(), sitemap()],
+  integrations: [icon(), sitemap(), mdx()],
   markdown: {
     syntaxHighlight: false
   }
