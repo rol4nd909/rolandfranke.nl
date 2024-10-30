d1890c2628f3116e726259821c48590e768f095e
Rol4nd <mail@rolandfranke.nl>
2024-10-30 12:04:40 +0100

    Added vercel adapter and update badge color

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,17 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+// import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rolandfranke.nl',
   integrations: [icon(), sitemap(), mdx()],
+
   markdown: {
     syntaxHighlight: false
-  }
+  },
+
+  // output: 'static',
+  // adapter: vercel()
 });
