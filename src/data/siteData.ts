946995b9a3becb2643b8876318aa5665ca8cff3e
Rol4nd <mail@rolandfranke.nl>
2024-11-12 10:34:58 +0100

    Added SEO

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -1,4 +1,4 @@
-export const baseData = {
+export const SITE = {
   title: 'Roland Franke - Frontend Developer',
   description:
     "Roland Franke's personal blog. Discover a creative frontend developer with expertise in HTML, CSS, React, and accessibility, dedicated to build inclusive and innovative web experiences. Explore projects, professional experience, and insights on modern web development.",
@@ -7,7 +7,7 @@ export const baseData = {
 export const socialLinks = [
   {
     name: 'X (twitter)',
-    url: 'https://twitter.com/rolandfranke',
+    url: 'https://x.com/rolandfranke',
     icon: 'icon-twitter',
   },
   {
