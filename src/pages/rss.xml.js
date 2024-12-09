a119978386713bb523ad11f7730693f15073d451
Rol4nd <mail@rolandfranke.nl>
2024-12-09 12:43:14 +0100

    Update pubdate in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,12 +12,20 @@ export async function GET(context) {
     site: context.site,
     trailingSlash: false,
     stylesheet: '/rss/styles.xsl',
-    items: formattedPosts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/frontend-stories/${post.id}`,
-    })),
+    items: formattedPosts.map((post) => {
+      const date = post.data.pubDate.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })
+
+      return ({
+        title: post.data.title,
+        pubDate: date,
+        description: post.data.description,
+        link: `/frontend-stories/${post.id}`,
+      })
+    }),
     customData: `<language>en-us</language>`,
   })
 }
