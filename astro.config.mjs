c62bd7dacbee8afd64b9379ce9030b7f1226f88c
Rol4nd <mail@rolandfranke.nl>
2025-04-16 09:01:26 +0200

    Remove unused astro-icon integration and replace icon components with SVG imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rolandfranke.nl',
-  integrations: [icon(), sitemap(), mdx()],
+  integrations: [sitemap(), mdx()],
   markdown: {
     syntaxHighlight: false
   },
